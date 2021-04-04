60e5312d67710717519c6be43bc5d8cf9864ebfe
pc24495 <pchummar42@gmail.com>
2021-04-04 16:25:39 -0400

    Initialized server and client

---
+++ client/src/Components/TestComponent.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import classes from "./TestComponent.module.css";
+
+export default class TestComponent extends Component {
+  render() {
+    return <div className={classes.TestComponent}>ASdfasdfas</div>;
+  }
+}
