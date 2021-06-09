cf41bb0138dc4c00815e2cd4fac6a1766fc93d9b
pc24495 <pchummar42@gmail.com>
2021-06-09 19:46:10 -0400

    Converted Feed Component to functional component

--- client/src/Components/MainSection/Feed/Feed.js
+++ client/src/Components/MainSection/Feed/Feed.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import classes from "./Feed.module.css";
 import Post from "../Post/Post.js";
 import axios from "axios";
@@ -6,97 +6,95 @@ import smile from "./smile.png";
 import Backdrop from "../../Backdrop/Backdrop.js";
 // import TextareaAutosize from "react-textarea-autosize";
 
-export default class Feed extends Component {
-  state = {
+export default function Feed(props) {
+  const [state, setState] = useState({
     imageArray: null,
     showImage: false,
     showBackdrop: false,
     showDropdowns: false,
-  };
+  });
 
-  showImageClick = () => {
-    axios.get("http://localhost:3000/api/getShowPosters").then((res) => {
-      console.log(res.data.image);
-      this.setState({ showImage: true, imageArray: res.data.image });
-    });
-  };
+  // const showImageClick = () => {
+  //   axios.get("http://localhost:3000/api/getShowPosters").then((res) => {
+  //     console.log(res.data.image);
+  //     this.setState({ showImage: true, imageArray: res.data.image });
+  //   });
+  // };
 
-  inputClickHandler = (event) => {
-    console.log(this.props);
-    this.setState({ showBackdrop: true });
+  const inputClickHandler = (event) => {
+    console.log(props);
+    setState({ ...state, showBackdrop: true });
     // this.props.history.push("/submitpost");
   };
 
-  handleBackdropClick = (event) => {
+  const handleBackdropClick = (event) => {
     if (event.target.className.includes("Backdrop_Backdrop")) {
-      this.setState({ showBackdrop: false });
+      setState({ ...state, showBackdrop: false });
     }
   };
 
-  onAnnouncementSelect = (event) => {
-    this.setState({ showDropdowns: false });
+  const onAnnouncementSelect = (event) => {
+    setState({ ...state, showDropdowns: false });
   };
 
-  onSpoilerSelect = (event) => {
-    this.setState({ showDropdowns: true });
+  const onSpoilerSelect = (event) => {
+    setState({ ...state, showDropdowns: true });
   };
 
-  tags = ["ShadowAndBone", "TestingTag", "TestingTag2"];
+  const tags = ["ShadowAndBone", "TestingTag", "TestingTag2"];
 
-  render() {
-    return (
-      <div className={classes.Feed}>
-        {this.state.showBackdrop ? (
-          <Backdrop
-            showBackdrop={this.state.showBackdrop}
-            onClick={(event) => this.handleBackdropClick(event)}
-          >
-            <div className={classes.Modal}>
-              <div className={classes.ModalTitle}>Enter post</div>
-              <textarea className={classes.PostInput}></textarea>
-              <div className={classes.RadioButtons}>
-                <div style={{ display: "inline-block" }}>
-                  <input
-                    name="Spoilers"
-                    type="radio"
-                    id="one"
-                    onChange={this.onAnnouncementSelect}
-                  ></input>
-                  <label for="one">Announcement</label>
-                </div>
-                <div style={{ display: "inline-block" }}>
-                  <input
-                    name="Spoilers"
-                    type="radio"
-                    id="two"
-                    onChange={this.onSpoilerSelect}
-                  ></input>
-                  <label for="two">Spoilers</label>
-                </div>
+  return (
+    <div className={classes.Feed}>
+      {state.showBackdrop ? (
+        <Backdrop
+          showBackdrop={state.showBackdrop}
+          onClick={handleBackdropClick}
+        >
+          <div className={classes.Modal}>
+            <div className={classes.ModalTitle}>Enter post</div>
+            <textarea className={classes.PostInput}></textarea>
+            <div className={classes.RadioButtons}>
+              <div style={{ display: "inline-block" }}>
+                <input
+                  name="Spoilers"
+                  type="radio"
+                  id="one"
+                  onChange={onAnnouncementSelect}
+                ></input>
+                <label for="one">Announcement</label>
               </div>
-              {this.state.showDropdowns ? (
-                <div className={classes.Dropdowns}>
-                  <select className={classes.Dropdown}></select>
-                  <select className={classes.Dropdown}></select>
-                  <select className={classes.Dropdown}></select>
-                </div>
-              ) : null}
+              <div style={{ display: "inline-block" }}>
+                <input
+                  name="Spoilers"
+                  type="radio"
+                  id="two"
+                  onChange={onSpoilerSelect}
+                ></input>
+                <label for="two">Spoilers</label>
+              </div>
             </div>
-          </Backdrop>
-        ) : null}
+            {state.showDropdowns ? (
+              <div className={classes.Dropdowns}>
+                <select className={classes.Dropdown}></select>
+                <select className={classes.Dropdown}></select>
+                <select className={classes.Dropdown}></select>
+              </div>
+            ) : null}
+          </div>
+        </Backdrop>
+      ) : null}
 
-        <div className={classes.InputPost}>
-          <img src={smile} className={classes.ProfilePic}></img>
-          <input
-            className={classes.InputPostInput}
-            placeholder="Submit a post..."
-            onClick={(event) => this.inputClickHandler(event)}
-          ></input>
-        </div>
-        <Post tags={this.tags}></Post>
-        <Post tags={this.tags}></Post>
-        <Post tags={this.tags}></Post>
+      <div className={classes.InputPost}>
+        <img src={smile} className={classes.ProfilePic}></img>
+        <input
+          className={classes.InputPostInput}
+          placeholder="Submit a post..."
+          onClick={inputClickHandler}
+        ></input>
       </div>
-    );
-  }
+      <Post tags={tags}></Post>
+      <Post tags={tags}></Post>
+      <Post tags={tags}></Post>
+    </div>
+  );
 }
